029122eca94c5c5a18e2232dc84dd8c7befe43eb
Marco091 <Marco91fire@gmail.com>
2024-02-09 16:50:22 +0100

    edit 5

--- config.php
+++ config.php
@@ -29,7 +29,7 @@ $mysqli->query('USE '. $nomedb); // Seleziono il DB
 
 
 
-// Creo la tabella
+// Creo la tabella se non esiste
 $sql = 'CREATE TABLE IF NOT EXISTS libri (
     `ID` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `titolo` VARCHAR(255) NOT NULL,
@@ -50,6 +50,8 @@ if (!$mysqli->query($sql)) {
 }
 
 
+// Creo la tabella utenti se non esiste
+
 $sql = 'CREATE TABLE IF NOT EXISTS utenti ( 
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     nome_utente VARCHAR(255) NOT NULL, 
@@ -58,5 +60,32 @@ $sql = 'CREATE TABLE IF NOT EXISTS utenti (
     
 )';
 if(!$mysqli->query($sql)) { die($mysqli->connect_error); }
+
+
+// Se il Db è vuoto , Creo qualche voce per non mostrare una pagina vuota.
+
+
+
+
+
+$result = $mysqli->query("SELECT COUNT(*) as total FROM libri");
+$data = $result->fetch_assoc();
+
+if ($data['total'] == 0) {
+    $sqlInsert = "INSERT INTO libri 
+    (`titolo`, `autore`, `anno`, `genere`, `prezzo`, `prezzo_offerta`, `spedizione`, `numero_recensioni`, `stelle`, `immagine`, `descrizione`) 
+VALUES 
+('Harry Potter e la pietra filosofale', 'J. K. Rowling', 1997, 'Fantasy', 21.15, 13.45, 'Spedizione Gratuita', 489, 4, 'uploads/Harry Potter e la pietra filosofale .jpeg', 'Girando la busta con mano tremante, Harry vide un sigillo di ceralacca color porpora con uno stemma araldico: un leone, un&#039;aquila, un tasso e un serpente intorno a una grossa H'),
+( 'Harry Potter e la camera dei segreti', 'J. K. Rowling', 1998, 'fantasy', 22.21, 10.99, 'Spedizione Gratuita', 479, 3, 'uploads/Harry Potter e la camera dei segreti .jpeg', '«C&#039;è un complotto, Harry Potter. Un complotto per far succedere le cose più terribili, quest&#039;anno, alla scuola di magia e stregoneria di Hogwarts».'),
+( 'the lord of rings', 'Tolkien', 2003, 'fantasy', 22.99, 19.99, 'Spedizione Gratuita', 256, 3, 'uploads/the lord of rings .jpeg', 'Since it was first published in 1954, The Lord of the Rings has been a book people have treasured.')
+   ;";
+
+if (!$mysqli->query($sqlInsert)) {
+    die($mysqli->error);
+}
+}
+
+
+
 
 ?>
